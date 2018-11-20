b6db09c22f7c87495236d03de1d9e332b327acb8
Andrés Becerra Gómez <abecerra@getinsoft.com>
2018-11-20 17:02:58 -0600

    componentes

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
+import { FormsModule  } from '@angular/forms';
+
+
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { GraphComponent } from './graph/graph.component';
+import { IncrementComponent } from '../components/increment/increment.component';
 
 // Routes
 import { PAGES_ROUTES } from './pages.routes';
@@ -11,22 +15,23 @@ import { PAGES_ROUTES } from './pages.routes';
 import { SharedModule } from '../shared/shared.module';
 
 
-
 @NgModule({
     declarations: [
         PagesComponent,
         DashboardComponent,
         ProgressComponent,
-        GraphComponent
+        GraphComponent,
+        IncrementComponent
     ],
     exports: [
         DashboardComponent,
         ProgressComponent,
-        GraphComponent
+        GraphComponent,
     ],
     imports: [
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        FormsModule
     ]
 })
 
