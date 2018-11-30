f3518e59fbfe021244366628d43c387924db486c
Andrés Becerra Gómez <abecerra@getinsoft.com>
2018-11-30 16:45:07 -0600

    sidebar complete

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,8 +3,15 @@ import { BreadcrumsComponent } from './breadcrums/breadcrums.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @NgModule({
+    imports: [
+        RouterModule,
+        CommonModule
+    ],
     declarations: [
         HeaderComponent,
         SidebarComponent,
